ecb3b6f92e115e2a571a619fa8893df9f52b6fe4
vianho <60379375+vianho@users.noreply.github.com>
2025-03-23 18:53:48 +0800

    chore: initial project bootstrap with Gradle

---
+++ src/main/java/sc2002/fcsi/grp3/Main.java
@@ -0,0 +1,17 @@
+package sc2002.fcsi.grp3;
+
+/**
+ * The {@code Main} class is the entry point of the application.
+ * This is a basic Hello World example used to demonstrate the structure
+ * of a Java application with JavaDoc comments.
+ */
+public class Main {
+    /**
+     * The main method which serves as the entry point of the application.
+     *
+     * @param args Command-line arguments passed to the program (not used here)
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
