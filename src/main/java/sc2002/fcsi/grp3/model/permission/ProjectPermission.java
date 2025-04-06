f1ab4e3a9b241e9a22637f178e09fca66feaf757
vianho <60379375+vianho@users.noreply.github.com>
2025-04-07 03:03:32 +0800

    feat: added permissions interfaces

---
+++ src/main/java/sc2002/fcsi/grp3/model/permission/ProjectPermission.java
@@ -0,0 +1,8 @@
+package sc2002.fcsi.grp3.model.permission;
+
+import sc2002.fcsi.grp3.model.Project;
+import sc2002.fcsi.grp3.model.User;
+
+public interface ProjectPermission {
+    boolean canCreateProject(User user, Project project);
+}
