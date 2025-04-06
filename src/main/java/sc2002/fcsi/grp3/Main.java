79575349928601493b64d48470183b35fc2e53e5
vianho <60379375+vianho@users.noreply.github.com>
2025-04-07 03:09:15 +0800

    feat: use init in main.java

--- src/main/java/sc2002/fcsi/grp3/Main.java
+++ src/main/java/sc2002/fcsi/grp3/Main.java
@@ -1,5 +1,12 @@
 package sc2002.fcsi.grp3;
 
+import sc2002.fcsi.grp3.controller.MainMenuController;
+import sc2002.fcsi.grp3.init.ControllerInitializer;
+import sc2002.fcsi.grp3.init.ViewInitializer;
+import sc2002.fcsi.grp3.io.ConfigLoader;
+import sc2002.fcsi.grp3.datastore.DataStore;
+import sc2002.fcsi.grp3.init.SystemInitializer;
+
 /**
  * The {@code Main} class is the entry point of the application.
  * This is a basic Hello World example used to demonstrate the structure
@@ -13,6 +20,16 @@ public class Main {
      * @param args Command-line arguments passed to the program (not used here)
      */
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
+        ConfigLoader config = new ConfigLoader("config.properties");
+        DataStore db = DataStore.getInstance();
+        SystemInitializer.initialize(db, config);
+
+        ViewInitializer viewInit = new ViewInitializer();
+        ControllerInitializer controllerInit = new ControllerInitializer(db, viewInit);
+
+        MainMenuController menu = controllerInit.getMainMenuController();
+        menu.start();
+
     }
 }
