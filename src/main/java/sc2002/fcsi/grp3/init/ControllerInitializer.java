d914b61c89952ae84e2a1496bb59a407ae57acad
vianho <60379375+vianho@users.noreply.github.com>
2025-04-07 03:08:34 +0800

    chore: added controller initializer

---
+++ src/main/java/sc2002/fcsi/grp3/init/ControllerInitializer.java
@@ -0,0 +1,18 @@
+package sc2002.fcsi.grp3.init;
+
+import sc2002.fcsi.grp3.controller.ControllerFactory;
+import sc2002.fcsi.grp3.controller.LoginController;
+import sc2002.fcsi.grp3.controller.MainMenuController;
+import sc2002.fcsi.grp3.datastore.DataStore;
+
+public class ControllerInitializer {
+    private final ControllerFactory factory;
+
+    public ControllerInitializer(DataStore store, ViewInitializer viewInit) {
+        this.factory = new ControllerFactory(store, viewInit);
+    }
+
+    public MainMenuController getMainMenuController() {
+        return factory.createMainMenuController();
+    }
+}
