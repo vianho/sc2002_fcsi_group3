df25c8c401de3b6b50615b894919d49ca04080bd
WA0001NO <wangstefano728@gmail.com>
2025-04-07 12:18:50 +0800

    Booking model and gitignore

--- src/main/java/sc2002/fcsi/grp3/model/Booking.java
+++ src/main/java/sc2002/fcsi/grp3/model/Booking.java
@@ -1,4 +1,46 @@
 package sc2002.fcsi.grp3.model;
 
+import java.time.LocalDate;
+
 public class Booking {
+    private int id;
+    private Flat flat;
+    private User applicant;
+    private User officer ; 
+    private Application application;
+    private LocalDate bookingDate;
+
+
+    public Booking(int id, Flat flat, User applicant, User officer, Application application, LocalDate bookingDate){
+        this.id = id;
+        this.flat = flat;
+        this.applicant = applicant;
+        this.officer = officer;
+        this.application = application;
+        this.bookingDate = bookingDate;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public Flat getFlat(){
+        return this.flat;
+    }
+
+    public User getApplicant(){
+        return this.applicant;
+    }
+
+    public User getOfficer(){
+        return this.officer;
+    }
+
+    public Application getApplication(){
+        return this.application;
+    }
+
+    public LocalDate getBookingDate(){
+        return this.bookingDate;
+    }
 }
